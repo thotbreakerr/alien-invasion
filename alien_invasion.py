68dd7b5b87e16a4d755964f8c475812ac64f8678
Jaroslav <jaroslavt101@yahoo.com>
2026-09-05 16:07:06 -0500

    add multiple rows of aliens to the fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,7 @@ def run_game():
     aliens = Group()
 
     # Create the fleet of aliens.
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Start the main loop for the game. If anything happens in game look at loop
     while True:
